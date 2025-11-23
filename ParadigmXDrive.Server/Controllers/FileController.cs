49b3c535706bf1d121da28168653eab632c51137
Crims0nR4ven <qisiel@protonmail.com>
2025-11-23 17:25:18 +0100

    Handle Large File Downloading Interruption

--- ParadigmXDrive.Server/Controllers/FileController.cs
+++ ParadigmXDrive.Server/Controllers/FileController.cs
@@ -34,8 +34,42 @@ namespace ParadigmXDrive.Server.Controllers
         [HttpGet("GetFileBlob")]
         public async Task<IActionResult> GetDownloadFile(string filePath)
         {
-            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 4096, useAsync: true);
-            return File(fileStream, "application/octet-stream", Path.GetFileName(filePath));
+            if (!System.IO.File.Exists(filePath))
+                return NotFound();
+
+            var fileInfo = new FileInfo(filePath);
+            var fileLength = fileInfo.Length;
+            var rangeHeader = Request.Headers["Range"].ToString();
+
+            if (string.IsNullOrEmpty(rangeHeader))
+                return PhysicalFile(filePath, "application/octet-stream", enableRangeProcessing: true);
+
+            // Parse range
+            var range = rangeHeader.Replace("bytes=", "").Split('-');
+            var start = long.Parse(range[0]);
+            var end = range.Length > 1 && !string.IsNullOrEmpty(range[1]) ? long.Parse(range[1]) : fileLength - 1;
+            var contentLength = end - start + 1;
+
+            Response.StatusCode = StatusCodes.Status206PartialContent;
+            Response.Headers.Add("Accept-Ranges", "bytes");
+            Response.Headers.Add("Content-Range", $"bytes {start}-{end}/{fileLength}");
+            Response.Headers.Add("Content-Length", contentLength.ToString());
+
+            using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            fs.Seek(start, SeekOrigin.Begin);
+            var buffer = new byte[64 * 1024]; // 64KB buffer
+
+            long remaining = contentLength;
+            while (remaining > 0)
+            {
+                var count = (int)Math.Min(buffer.Length, remaining);
+                var read = await fs.ReadAsync(buffer, 0, count);
+                if (read == 0) break;
+                await Response.Body.WriteAsync(buffer.AsMemory(0, read));
+                remaining -= read;
+            }
+
+            return new EmptyResult();
         }
 
         [HttpPatch("UpdateFilePath")]
